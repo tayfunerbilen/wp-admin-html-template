23b0cfd58f45e8b3063405d8ea1d762771a274e4
tayfunerbilen <tayfunerbilen@gmail.com>
2016-03-30 17:15:17 +0200

    tema dosyaları

---
+++ assets/scripts/admin.js
@@ -0,0 +1,86 @@
+/**
+ * @author      Tayfun Erbilen
+ * @web         http://erbilen.net
+ * @mail        tayfunerbilen@gmail.com
+ */
+$(function () {
+
+    $('.box >h3').append('<button type="button" class="toggle"><span class="fa fa-caret-up"></span></button>');
+
+    $(document).on('click', 'button.toggle', function (e) {
+        var id = $(this).closest('.box-container').attr('id');
+        $(this).parent().next().toggle();
+        if ($('.fa', this).hasClass('fa-caret-up')) {
+            $('.fa', this).removeClass('fa-caret-up').addClass('fa-caret-down');
+            if (id != 'undefined') {
+                localStorage.setItem('box_' + id, true);
+            }
+        } else {
+            $('.fa', this).removeClass('fa-caret-down').addClass('fa-caret-up');
+            if (id != 'undefined') {
+                delete localStorage['box_' + id];
+            }
+        }
+        e.preventDefault();
+    });
+    function checkToggle() {
+        $.each(localStorage, function (key, val) {
+            if (!key.indexOf('box_')) {
+                $('#' + (key.replace('box_', '')) + ' .toggle').trigger('click');
+            }
+        });
+    }
+
+    checkToggle();
+
+    $('.sidebar >ul >li:not(.line)').hover(function () {
+        if (!$('.sub-menu:visible', this).length) {
+            $('.dropdown-menu', this).show();
+            $(this).addClass('hover');
+        }
+    }, function () {
+        $('.dropdown-menu', this).hide();
+        $(this).removeClass('hover');
+    });
+
+    $('[dropdown] >li').hover(function () {
+        $('ul', this).show();
+        $(this).addClass('active');
+    }, function () {
+        $('ul', this).hide();
+        $(this).removeClass('active');
+    });
+
+    $('.sidebar >ul >li').each(function () {
+        if ($('.sub-menu', this).length) {
+            var html = $('.sub-menu', this).html();
+            $(this).append('<ul dropdown class="dropdown-menu">' + html + '</ul>');
+        }
+    });
+
+    $('.collapse-menu').on('click', function (e) {
+        $('.sidebar').toggleClass('fix');
+        if ($('.fa', this).hasClass('fa-arrow-circle-left')) {
+            $('.sidebar >ul >li.active .sub-menu').hide();
+            $('.fa', this).removeClass('fa-arrow-circle-left').addClass('fa-arrow-circle-right');
+            localStorage.setItem('sidebar', true);
+        } else {
+            $('.fa', this).removeClass('fa-arrow-circle-right').addClass('fa-arrow-circle-left');
+            $('.sidebar >ul >li.active .sub-menu').show();
+            delete localStorage['sidebar'];
+        }
+        e.preventDefault();
+    });
+    function sidebarCheck() {
+        if (localStorage.getItem('sidebar')) {
+            $('.sidebar .collapse-menu').trigger('click');
+        }
+    }
+
+    sidebarCheck();
+
+    if ($('#editor').length) {
+        CKEDITOR.replace('editor');
+    }
+
+});
